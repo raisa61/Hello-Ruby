dff3ab80a07efe3dd88a8fc6003ef8d439caacdd
raisa61 <60297428+raisa61@users.noreply.github.com>
2021-01-01 17:58:55 -0500

    Update BASICS.rb

--- INTRO TO RUBY/NUMBERS/BASICS.rb
+++ INTRO TO RUBY/NUMBERS/BASICS.rb
@@ -26,3 +26,10 @@ y= "10".to_f #string to float
 puts x*y
 puts "hello".to_i #gives 0, because hello isn't convertible to an int
 puts " "
+
+#COMPARISON
+puts "hello"=="hello" #true
+puts "" == " " #false
+puts 10== "10".to_f #true
+puts 10===10.0 #true
+puts 10.eql?(10.0) #false because .eql? compares the types
