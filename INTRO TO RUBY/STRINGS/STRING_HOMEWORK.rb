715bdc5db778dfe54c637eb66ffe4157261a0d0b
raisa61 <60297428+raisa61@users.noreply.github.com>
2021-01-01 13:08:18 -0500

    taking user inputs and displaying it

---
+++ INTRO TO RUBY/STRINGS/STRING_HOMEWORK.rb
@@ -0,0 +1,8 @@
+#THE HOMEWORK PRACTICE
+puts "Pleas enter your first name"
+first=gets.chomp
+puts "Pleas enter your last name"
+last=gets.chomp
+puts "Thanks! your full name is #{first} #{last}"
+length_without_space= first.length + last.length
+puts "Your name has #{length_without_space} characters in it"
