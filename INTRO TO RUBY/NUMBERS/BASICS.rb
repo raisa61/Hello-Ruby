b784c0eb54394a185a8f2ce166d857e41f78661d
raisa61 <60297428+raisa61@users.noreply.github.com>
2021-01-01 13:47:54 -0500

    how numbers and strings work

---
+++ INTRO TO RUBY/NUMBERS/BASICS.rb
@@ -0,0 +1,28 @@
+# CAN DO OPERATIONS DIRECTLY IN PUTS
+puts 1 + 2 # 3
+puts 10/4 # 2 (casts the answer to an int)
+puts 10/4.to_f # 2.5 (gives the actual ans, because we just converted 4 to float)
+puts (10/4).to_f # 2.0
+puts " "
+
+#TESTING WITH STRING
+puts "5"*2 # 55
+#puts "5"*"5" #error (bc we're doing string into string)
+puts " "
+
+#PRINTING A LINE
+puts "-"*30 #fun!
+#or you can do it with the TIMES method
+30.times {print "-"}
+puts " "
+
+#GENERATING RANDOM NUMBERS
+20.times {puts rand(10)} #rand(10) generates random numbers between 0 and 9
+puts " "
+
+#CONVERTING STRING TO NUMBERS
+x= "5".to_i #string to int
+y= "10".to_f #string to float
+puts x*y
+puts "hello".to_i #gives 0, because hello isn't convertible to an int
+puts " "
