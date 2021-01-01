d3585c72a5b2c05adf0e9005165ac0efc0373750
raisa61 <60297428+raisa61@users.noreply.github.com>
2021-01-01 17:45:19 -0500

    Update homework.rb

--- INTRO TO RUBY/NUMBERS/homework.rb
+++ INTRO TO RUBY/NUMBERS/homework.rb
@@ -1,7 +1,9 @@
 
 #A SIMPLE CALCULATOR THAT DOES ALL THE BASIC MATHEMATICAL OPERATIONS
 #it's better to not modify the user inputs directly in the chomp operation
-
+def multiply(a,b)
+  a.to_f*b.to_f
+end
 20.times {print "-"}
 puts " "
 puts "A SIMPLE CALCULATOR"
@@ -11,7 +13,7 @@ puts "Please enter the first number"
 first=gets.chomp
 puts "Please enter the second number"
 second=gets.chomp
-puts "The two numbers multipled, gives us #{first.to_f*second.to_f}"
+puts "The two numbers multipled, gives us #{multiply(first,second)}"
 puts "The two numbers added, gives us #{first.to_f+second.to_f}"
 puts "The two numbers substracted, gives us #{first.to_f-second.to_f}"
 puts "The two numbers divided, gives us #{first.to_f/second.to_f}"
