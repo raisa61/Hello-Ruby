1ad20ea26f083f03aba5ea6447a7c1399f015478
raisa61 <60297428+raisa61@users.noreply.github.com>
2021-01-01 13:14:44 -0500

    Update STRING_HOMEWORK.rb

--- INTRO TO RUBY/STRINGS/STRING_HOMEWORK.rb
+++ INTRO TO RUBY/STRINGS/STRING_HOMEWORK.rb
@@ -1,8 +1,11 @@
-#THE HOMEWORK PRACTICE
-puts "Pleas enter your first name"
+#takes user input for first and last names and then just shows the full name, the number of alphabets, and the name in reverse
+puts "Please enter your first name"
 first=gets.chomp
 puts "Pleas enter your last name"
 last=gets.chomp
-puts "Thanks! your full name is #{first} #{last}"
+full_name = first + " " + last
+puts "Thanks! your full name is #{full_name}"
 length_without_space= first.length + last.length
 puts "Your name has #{length_without_space} characters in it"
+puts "Your name in reverse is #{full_name.reverse}"
+
